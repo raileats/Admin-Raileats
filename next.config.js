753c6378db57c0e9dcbb47dc30e30dd3095f73b7
raileats <raileats@gmail.com>
2025-09-07 00:48:20 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
+async rewrites() {
+  return [
+    {
+      source: '/',
+      has: [{ type: 'host', value: 'admin.raileats.in' }],
+      destination: '/admin'
+    }
+  ];
+}
