1823c6ac4d7f4b8bb55f5c5fa6ca1539f5678298
raileats <raileats@gmail.com>
2025-09-07 00:49:24 +0530

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function AdminRoot() {
-  // You can check server side auth here (cookies) and redirect to login if needed.
-  // For simple: redirect to /admin/home
   redirect("/admin/home");
 }
