176a795b28f0738b3b6aeccbbdf9d351a10d159f
raileats <raileats@gmail.com>
2025-09-07 01:14:10 +0530

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,2 @@
-// app/admin/page.tsx
 import { redirect } from "next/navigation";
-
-export default function AdminRoot() {
-  // If you want to show admin home directly:
-  redirect("/admin/home");
-}
+export default function AdminRoot() { redirect("/admin/home"); }
