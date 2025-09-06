246a7a58bb023807fbbee6d7881490062e49763c
raileats <raileats@gmail.com>
2025-09-07 01:13:20 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,4 @@
-// app/layout.tsx
 import "./globals.css";
-
-export const metadata = {
-  title: "RailEats",
-  description: "RailEats Admin Panel",
-};
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
