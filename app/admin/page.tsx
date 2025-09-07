53e4472ec2612056d151d1c2a216237d858c2604
raileats <raileats@gmail.com>
2025-09-07 10:24:27 +0530

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,7 @@
 // app/admin/page.tsx
 import { redirect } from "next/navigation";
+
 export default function AdminRoot() {
-  redirect("/admin/home");
+  // send user to login by default; middleware will also handle auth checks
+  redirect("/admin/login");
 }
