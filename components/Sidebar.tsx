886441e6e2a0dd4539f8b902e67cef4c117ad466
raileats <raileats@gmail.com>
2025-09-12 06:01:57 +0530

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,9 +8,10 @@ export default function Sidebar() {
       style={{
         width: 240,
         padding: 20,
-        background: "#000", // black sidebar background
-        color: "#fff", // default white text
+        background: "#fff", // white background
+        color: "#000", // default black text
         minHeight: "100vh",
+        borderRight: "1px solid #e6e8eb",
       }}
     >
       {/* Logo + RailEats */}
@@ -42,7 +43,7 @@ export default function Sidebar() {
           }}
         >
           <span style={{ color: "#F6C800" }}>Rail</span>
-          <span style={{ color: "#fff" }}>Eats</span>
+          <span style={{ color: "#000" }}>Eats</span>
         </h3>
       </div>
 
@@ -50,42 +51,45 @@ export default function Sidebar() {
       <nav>
         <ul style={{ listStyle: "none", padding: 0, margin: 0 }}>
           <li style={{ marginBottom: 12 }}>
-            <Link href="/admin/home" style={{ color: "#fff", textDecoration: "none" }}>
+            <Link href="/admin/home" style={{ color: "#000", textDecoration: "none" }}>
               Home
             </Link>
           </li>
           <li style={{ marginBottom: 12 }}>
-            <Link href="/admin/orders" style={{ color: "#fff", textDecoration: "none" }}>
+            <Link href="/admin/orders" style={{ color: "#000", textDecoration: "none" }}>
               Orders
             </Link>
           </li>
           <li style={{ marginBottom: 12 }}>
-            <Link href="/admin/menu" style={{ color: "#fff", textDecoration: "none" }}>
+            <Link href="/admin/menu" style={{ color: "#000", textDecoration: "none" }}>
               Menu
             </Link>
           </li>
           <li style={{ marginBottom: 12 }}>
-            <Link href="/admin/trains" style={{ color: "#fff", textDecoration: "none" }}>
+            <Link href="/admin/trains" style={{ color: "#000", textDecoration: "none" }}>
               Trains
             </Link>
           </li>
           <li style={{ marginBottom: 12 }}>
-            <Link href="/admin/stations" style={{ color: "#fff", textDecoration: "none" }}>
+            <Link href="/admin/stations" style={{ color: "#000", textDecoration: "none" }}>
               Stations
             </Link>
           </li>
           <li style={{ marginBottom: 12 }}>
-            <Link href="/admin/users" style={{ color: "#fff", textDecoration: "none" }}>
+            <Link href="/admin/users" style={{ color: "#000", textDecoration: "none" }}>
               Users
             </Link>
           </li>
           <li style={{ marginBottom: 12 }}>
-            <Link href="/admin/vendors" style={{ color: "#fff", textDecoration: "none" }}>
+            <Link href="/admin/vendors" style={{ color: "#000", textDecoration: "none" }}>
               Vendors
             </Link>
           </li>
           <li style={{ marginTop: 20 }}>
-            <Link href="/admin/logout" style={{ color: "red", fontWeight: "bold", textDecoration: "none" }}>
+            <Link
+              href="/admin/logout"
+              style={{ color: "red", fontWeight: "bold", textDecoration: "none" }}
+            >
               Logout
             </Link>
           </li>
