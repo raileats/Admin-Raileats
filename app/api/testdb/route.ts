8251b86eb59810d3d082022d3937c4799f5df0fd
raileats <raileats@gmail.com>
2025-09-14 18:56:10 +0530

    Update route.ts

--- app/api/testdb/route.ts
+++ app/api/testdb/route.ts
@@ -1,6 +1,4 @@
 import { db } from "@/lib/db";
-
-export async function GET() {
   const { data, error } = await db.from("Stations").select("*").limit(1);
   return Response.json({ data, error });
 }
