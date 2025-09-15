5dad9f7e97e478481265d5e95809058b43eb2473
raileats <raileats@gmail.com>
2025-09-16 04:40:00 +0530

    Update page.tsx

--- app/admin/restros/page.tsx
+++ app/admin/restros/page.tsx
@@ -1,11 +1,11 @@
 import dynamic from 'next/dynamic';
-const RestroList = dynamic(() => import('@/components/admin/RestroList'), { ssr: false });
+const RestroMasterList = dynamic(()=>import('@/components/admin/RestroMasterList'), { ssr:false });
 
-export default function RestrosPage() {
+export default function RestroMasterPage() {
   return (
     <div className="p-6">
       <h1 className="text-2xl mb-4">Restro Master</h1>
-      <RestroList />
+      <RestroMasterList />
     </div>
   );
 }
