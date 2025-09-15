d8dd6208d8d63044c1ec59531049a0b70e605ac7
raileats <raileats@gmail.com>
2025-09-15 21:41:44 +0530

    Create route.ts

---
+++ app/api/stations/route.ts
@@ -0,0 +1,4 @@
+const cols = [
+  'StationId','StationName','StationCode','Category','EcatRank','Division',
+  'RailwayZone','EcatZone','District','State','Lat','Long','Address','ReGroup','is_active'
+].join(',');
