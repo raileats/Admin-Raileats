933552494cec1676233782f97b2d1ad85c0d6a57
raileats <raileats@gmail.com>
2025-10-08 00:56:46 +0530

    Update TabContainer.tsx

--- components/TabContainer.tsx
+++ components/TabContainer.tsx
@@ -7,6 +7,10 @@ type Props = {
   style?: React.CSSProperties;
 };
 
+/**
+ * Simple wrapper used by tab components.
+ * IMPORTANT: Tab components should NOT render their own centered headings.
+ */
 export default function TabContainer({ children, className = "", style }: Props) {
   return (
     <div className={`tab-container ${className}`} style={style}>
