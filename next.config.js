003bbd385b959bdab6215117b08fcdaa9bf7a832
raileats <raileats@gmail.com>
2025-10-14 00:11:13 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** next.config.js */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  // Agar tumhe basePath, images, env vars ya other settings chahiye toh yahan add karo.
-};
-
-module.exports = nextConfig;
