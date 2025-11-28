f432a3d565c83dea6a800105e21f746192352fd4
raileats <raileats@gmail.com>
2025-11-28 23:48:36 +0530

    Update route.ts

--- app/api/admin/trains/[trainId]/route.ts
+++ app/api/admin/trains/[trainId]/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
-import { serviceClient } from "../../../../lib/supabaseServer";
+import { serviceClient } from "../../../../../lib/supabaseServer";
+
 
 type TrainRouteRow = {
   id?: number; // agar table me PK ho to
