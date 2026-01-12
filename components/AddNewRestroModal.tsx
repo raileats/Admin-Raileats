904ab4c1a337899e932f4dc81fddf9914c2f3588
raileats <raileats@gmail.com>
2026-01-12 22:44:14 +0530

    Update AddNewRestroModal.tsx

--- components/AddNewRestroModal.tsx
+++ components/AddNewRestroModal.tsx
@@ -1,1 +1,19 @@
+"use client";
+
+import React from "react";
+import RestroEditModal from "@/components/RestroEditModal";
+
+type Props = {
+  onClose: () => void;
+};
+
+export default function AddNewRestroModal({ onClose }: Props) {
+  return (
+    <RestroEditModal
+      restro={null}                // 🔥 IMPORTANT: null = NEW RESTRO
+      onClose={onClose}
+      initialTab="Basic Information"
+    />
+  );
+}
 
