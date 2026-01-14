f181af97fc919c3e52c652af0652aefefd78ace5
raileats <raileats@gmail.com>
2026-01-15 00:09:14 +0530

    Update page.tsx

--- app/admin/restros/[code]/edit/contacts/page.tsx
+++ app/admin/restros/[code]/edit/contacts/page.tsx
@@ -1,29 +1,5 @@
 import ContactsClient from './ContactsClient';
 
-type Item = {
-  id: string;
-  name: string;
-  value: string;
-  active: boolean;
-};
-
-export default function Page({
-  params,
-}: {
-  params: { code: string };
-}) {
-  const restroCode = params.code;
-
-  // Abhi ke liye empty data
-  // baad me yahan API call lagegi
-  const initialEmails: Item[] = [];
-  const initialWhatsapps: Item[] = [];
-
-  return (
-    <ContactsClient
-      restroCode={restroCode}
-      initialEmails={initialEmails}
-      initialWhatsapps={initialWhatsapps}
-    />
-  );
+export default function Page() {
+  return <ContactsClient />;
 }
