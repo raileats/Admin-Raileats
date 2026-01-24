faf9b2f182db16dfc99157967975d7ca5864e51e
raileats <raileats@gmail.com>
2026-01-24 22:04:09 +0530

    Add AdminSection component with title and action props

--- components/AdminSection.tsx
+++ components/AdminSection.tsx
@@ -1,1 +1,23 @@
+"use client";
+
+import React from "react";
+
+type Props = {
+  title: string;
+  action?: React.ReactNode;
+  children: React.ReactNode;
+};
+
+export default function AdminSection({ title, action, children }: Props) {
+  return (
+    <div className="border rounded-md p-4 bg-sky-50 mb-6">
+      <div className="flex justify-between items-center mb-3">
+        <h4 className="font-semibold text-sm">{title}</h4>
+        {action}
+      </div>
+
+      {children}
+    </div>
+  );
+}
 
