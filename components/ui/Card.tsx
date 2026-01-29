1f162c04d2bc6b07ea1bb156b0cde359ea129e79
raileats <raileats@gmail.com>
2026-01-30 00:10:58 +0530

    Update Card.tsx

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -1,18 +1,14 @@
 import React from "react";
-import clsx from "clsx";
 
 type Props = {
   children: React.ReactNode;
   className?: string;
 };
 
-export default function Card({ children, className }: Props) {
+export default function Card({ children, className = "" }: Props) {
   return (
     <div
-      className={clsx(
-        "rounded-xl border bg-white p-6",
-        className
-      )}
+      className={`rounded-xl border bg-white p-6 ${className}`}
     >
       {children}
     </div>
