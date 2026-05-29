4edd271c68f6f746099de75a02b6084c29908371
raileats <raileats@gmail.com>
2026-05-29 14:38:46 +0530

    Create page.tsx

---
+++ app/admin/restros/new/contacts/page.tsx
@@ -0,0 +1,13 @@
+// app/admin/restros/new/contacts/page.tsx
+"use client";
+
+import NewRestroCodeGate from "@/components/restro-route-tabs/NewRestroCodeGate";
+import ContactsClient from "@/components/tabs/ContactsClient";
+
+export default function NewContactsPage() {
+  return (
+    <NewRestroCodeGate>
+      {() => <ContactsClient />}
+    </NewRestroCodeGate>
+  );
+}
