b7a309632f744570eb2f98955469a20cdd918209
raileats <raileats@gmail.com>
2026-05-30 02:34:33 +0530

    Update route.ts

--- app/api/restrosmaster/route.ts
+++ app/api/restrosmaster/route.ts
@@ -78,6 +78,7 @@ export async function PATCH(req: Request) {
       "RestroEmail",
       "RestroPhone",
       "RestroRating",
+      "IsPureVeg",
       "RestroDisplayPhoto",
       "RaileatsStatus",
       "IsIrctcApproved",
