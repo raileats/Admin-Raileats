2921aa9608dd0e7384dcdfb7dd98b8c4baf05ef1
raileats <raileats@gmail.com>
2026-05-29 13:54:45 +0530

    Delete app/admin/restros/new/statio-settings directory

--- app/admin/restros/new/statio-settings/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/admin/restros/new/station-settings/page.tsx
-"use client";
-
-import NewRestroCodeGate from "@/components/restro-route-tabs/NewRestroCodeGate";
-import StationSettingsClient from "@/components/restro-route-tabs/StationSettingsClient";
-
-export default function NewStationSettingsPage() {
-  return <NewRestroCodeGate>{(code) => <StationSettingsClient restroCode={code} mode="new" />}</NewRestroCodeGate>;
-}
