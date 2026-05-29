9c21173a0040ee0350815faaa60bfa8285e857f0
raileats <raileats@gmail.com>
2026-05-29 19:20:27 +0530

    Update page.tsx

--- app/admin/restros/new/contacts/page.tsx
+++ app/admin/restros/new/contacts/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 // app/admin/restros/new/contacts/page.tsx
 import ContactsClient from "@/components/tabs/ContactsClient";
 import NewRestroCodeGate from "@/components/restro-route-tabs/NewRestroCodeGate";
