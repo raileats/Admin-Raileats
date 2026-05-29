57b975a8523ed2cce9c2d5845ce8a90005b51410
raileats <raileats@gmail.com>
2026-05-29 14:38:28 +0530

    Delete app/admin/restros/new/contacts.tsx

--- app/admin/restros/new/contacts.tsx
+++
@@ -1,6 +0,0 @@
-// app/admin/restros/new/contacts/page.tsx
-import ContactsClient from "@/components/tabs/ContactsClient";
-
-export default function NewContactsPage() {
-  return <ContactsClient />;
-}
