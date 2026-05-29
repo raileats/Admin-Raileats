7f35d972ac9720ef5295743c4cea7b6e5cea1cc1
raileats <raileats@gmail.com>
2026-05-29 18:57:22 +0530

    Update page.tsx

--- app/admin/restros/[code]/edit/contacts/page.tsx
+++ app/admin/restros/[code]/edit/contacts/page.tsx
@@ -1,5 +1,6 @@
-import ContactsClient from './ContactsClient';
+// app/admin/restros/[code]/edit/contacts/page.tsx
+import ContactsClient from "@/components/tabs/ContactsClient";
 
-export default function Page() {
-  return <ContactsClient />;
+export default function ContactsPage({ params }: { params: { code: string } }) {
+  return <ContactsClient restroCode={params.code} />;
 }
