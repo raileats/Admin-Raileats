95280842618664f680ef91d30661d9d18961e5e5
raileats <raileats@gmail.com>
2026-07-07 00:33:12 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 // app/layout.tsx
 import "./globals.css";
 import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export const metadata = {
   title: "RailEats Admin",
