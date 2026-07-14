95042ea30f920d64d84b9e13a65fed72341ba141
raileats <raileats@gmail.com>
2026-07-15 01:18:13 +0530

    Create RestroRdsTable.tsx

---
+++ components/admin/RestroRdsTable.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+
+export default function RestroRdsTable() {
+  return (
+    <div className="p-10 text-center text-slate-500">
+      Loading Restro RDS...
+    </div>
+  );
+}
